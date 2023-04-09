82366e53eaa51d7bbdbe078257d56c8744efa715
Yurii Koval <30389972+ykoval201@users.noreply.github.com>
2023-04-08 21:37:04 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    srand(1);
+
+    printf("Welcome to the game of TwentyFour Part Two!\n");
+	printf("Use each of the four numbers shown exactly once, \n");
+	printf("combining them somehow with the basic mathematical operators (+,-,*,/) \n");
+	printf("to yield the value twenty-four.\n");
+    printf("Choose your difficulty level: E for easy, M for medium, H for hard (default is easy).\n");
+    printf("Your choice --> ");
+    char difficulty;
+    scanf("%c", &difficulty);
+    
+
+
+}
---
+++ prog3-starter-code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf("Welcome to the game of TwentyFour Part Two!\n");
+	printf("Use each of the four numbers shown exactly once, \n");
+	printf("combining them somehow with the basic mathematical operators (+,-,*,/) \n");
+	printf("to yield the value twenty-four.\n");
+
+	printf("The numbers to use are: 1, 2, 3, 4");
+
+	printf("Enter your solution: ");
+
+	printf("Error! Invalid symbol entered. Please try again.\n\n");
+
+	printf("Error! You must use all four numbers, and use each one only once. Please try again.\n\n");
+
+	printf("Enter: \t1 to play again,\n");
+	printf("\t2 to change the difficulty level and then play again, or\n");
+	printf("\t3 to exit the program.\n");
+	printf("Your choice --> ");
+
+	printf("\nThanks for playing!\n");
+	printf("Exiting...\n");
+	
+	return 0;
+}
